aa4c90d836967ebfabf5530bc30fcd4cf27c3e49
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-09 14:39:28 +0100

    use Twig templates to render information

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,12 +3,13 @@
 namespace  App\Controller; // Every controller should have a name space that
 // starts with `App\`
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // The class name and the file name should be the same and there should be no
 // more than one class per file
-class HelloController
+class HelloController extends AbstractController // The AbstractController helps to generate based on a Twig template this why we do the extend
 {
     private array $messages = [
         'Hello', 'Hi', 'Bye!'
@@ -17,14 +18,22 @@ class HelloController
     #[Route('/{limit<\d+>?3}', name: 'app_index')] // Always give a `name` to the routes
     public function index(int $limit): Response
     {
-        return new Response(
-            implode(',', array_slice($this->messages, 0, $limit))
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'message' => implode(',', array_slice($this->messages, 0, $limit))
+            ]
         );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')] // A route with parameter
     public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
     }
 }
