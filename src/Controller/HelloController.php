ba69fae864d579f1bbbb0499b501ea512d700078
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-09 12:36:33 +0100

    feat: use php8 attributes to creat routes (it's the best way)

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,13 +4,25 @@ namespace  App\Controller; // Every controller should have a name space that
 // starts with `App\`
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 // The class name and the file name should be the same and there should be no
 // more than one class per file
 class HelloController
 {
+    private array $messages = [
+        'Hello', 'Hi', 'Bye!'
+    ];
+
+    #[Route('/', name: 'app_index')] // Always give a `name` to the routes
     public function index(): Response
     {
-        return new Response("HI!");
+        return new Response(implode(',', $this->messages));
+    }
+
+    #[Route('/messages/{id}', name: 'app_show_one')] // A route with parameter
+    public function showOne($id): Response
+    {
+        return new Response($this->messages[$id]);
     }
 }
