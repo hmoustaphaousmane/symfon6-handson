0e8e59cc4cd29d139277f23c1f0512b362a2bed9
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-10 15:16:33 +0100

    symfony forms

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -6,6 +6,7 @@ use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +25,21 @@ class MicroPostController extends AbstractController
     {
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    #[Route('micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add() : Response {
+        $micropost = new MicroPost();
+        // Create a form using form builder and configure it to handle the properties of the MicroPost entity (`title` and `text`)
+        $form = $this->createFormBuilder($micropost)
+            ->add('title')
+            ->add('text')
+            ->add('submit', SubmitType::class, ['label' => 'Save'])
+            ->getForm(); // Retrieve the form
+
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form
         ]);
     }
 }
