5ecaf5f45256a724edc504f28c3856f5e91d8160
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-17 15:00:35 +0100

    docs: working with user roles

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MicroPostController extends AbstractController
 {
@@ -34,8 +35,15 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function add(Request $request, EntityManagerInterface $entityManager) : Response
     {
+        // `denyAccessUnlessGranted()` method protect controller action from
+        // unauthorised use. This does the samething as the pHp8 attirbute
+        // `#[IsGranted('IS_AUTHENTICATED_FULLY')]`
+        // $this->denyAccessUnlessGranted(
+        //     'IS_AUTHENTICATED_FULLY'
+        // );
         // dd($this->getUser());
         $form = $this->createForm(MicroPostType::class, new MicroPost());
         $form->handleRequest($request);
@@ -66,6 +74,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    #[IsGranted('ROLE_EDITOR')]
     public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);
@@ -88,6 +97,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    #[IsGranted('ROLE_COMMENTER')]
     public function addComment(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(CommentType::class, new Comment());
