eb6ce8ff3bc09a7caa285edbfb342f14609e9918
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-17 15:53:28 +0100

    feat: manage actions persions using Voters

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -26,6 +26,7 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    #[IsGranted(MicroPost::VIEW, 'post')]
     public function showOne(MicroPost $post) : Response
     {
         return $this->render('micro_post/show.html.twig', [
@@ -72,11 +73,13 @@ class MicroPostController extends AbstractController
     }
 
     #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
-    #[IsGranted('ROLE_EDITOR')]
+    #[IsGranted(MicroPost::EDIT, 'post')]
     public function edit(MicroPost $post, Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MicroPostType::class, $post);
         $form->handleRequest($request);
+
+        // $this->denyAccessUnlessGranted(MicroPost::EDIT, 'post');
 
         if ($form->isSubmitted() && $form->isValid()) {
             $post = $form->getData();
