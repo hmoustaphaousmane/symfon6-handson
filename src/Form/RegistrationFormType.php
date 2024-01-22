38c5f5200c992be0fe6085f3c5922a015b4631ae
hmoustaphaousmane <hmoustaphaousmane@gmail.com>
2024-01-22 15:33:02 +0100

    feat: add profile settings ling and embed user profile form inside registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,15 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -57,6 +58,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('userProfile', UserProfileType::class)
         ;
     }
 
